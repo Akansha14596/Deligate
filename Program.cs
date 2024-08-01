e97c68706ab5839fc1bdd5332fa51eac6b2e980f
Akansha Mittal <akansha.goel@sopratseria.com>
2024-08-01 11:29:40 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace deligatePractice
+{
+    public delegate int AddDeligate(int x, int y);
+    class Program
+    {
+        public static int AddNum(int a ,int b)
+        {
+            return a + b;
+        }
+
+        public static string SayHello(string name)
+        {
+            return "Hello " + name;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddNum(1, 2));
+            Console.WriteLine(SayHello("Akansha"));
+        }
+    }
+}
